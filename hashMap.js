f17a2abc98b426f5397256b383412a789c9f1bf5
mikeymanoguerra <mikeymanoguerra@gmail.com>
2019-03-04 12:39:55 -0500

    hashmap.js built

---
+++ hashMap.js
@@ -0,0 +1,83 @@
+'use strict';
+
+class HashMap {
+  constructor(initialCapacity = 8) {
+    this.length = 0;
+    this._slots = [];
+    this._capacity = initialCapacity;
+    this._deleted = 0;
+  }
+  
+  get(key){
+    const index = this._findSlot(key);
+    const slot  = this._slots[index];
+    if(slot === undefined){
+      throw new Error('key error, cannot get');
+    }
+    return slot.value;
+  }
+
+  set(key, value) {
+    if ((this.length + 1) / this._capacity > HashMap.MAX_LOAD_RATIO) {
+      this._resize(this._capacity * HashMap.SIZE_RATIO);
+    }
+    const index = this._findSlot(key);
+    this._slots[index] = {
+      key,
+      value,
+      deleted: false
+    };
+    this.length++;
+  }
+  remove(key) {
+    const index = this._findSlot(key);
+    const slot = this._slots[index];
+    if (slot === undefined) {
+      throw new Error('Key Error');
+    }
+
+    slot.deleted = true;
+    this.length--;
+    this._deleted++;
+  }
+
+  static _hashString(string) {
+    let hash = 5381;
+    for (let i = 0; i < string.length; i++) {
+      hash = (hash << 5) + hash + string.charCodeAt(i);
+      hash = hash & hash;
+    }
+    return hash >>> 0;
+  }
+
+  _findSlot(key) {
+    const hash = HashMap._hashString(key);
+    const start = hash % this._capacity;
+
+    for (let i = start; i < start + this._capacity; i++) {
+      const index = i % this._capacity;
+      const slot = this._slots[index];
+      if (slot === undefined || slot.key === key) {
+        return index;
+      }
+    }
+  }
+
+ 
+
+  _resize(size) {
+    const oldSlots = this._slots;
+    this._capacity = size;
+    this.length = 0;
+    this._slots = [];
+
+    for (let slot of oldSlots) {
+      if (slot !== undefined) {
+        this.set(slot.key, slot.value);
+      }
+    }
+  }
+}
+
+HashMap.MAX_LOAD_RATIO = 0.9;
+HashMap.SIZE_RATIO = 3;
